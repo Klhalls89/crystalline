95e20e6dae40ab7cbca34074b09a86725c7e621a
Kristen Hallstrom <klhalls89@gmail.com>
2019-06-10 17:03:22 -0600

    Setup variables for app

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-
-function App() {
-  return (
-    <div className="App">
-      <h1>crystalline</h1>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      gems: []
+    }
+  }
+  render(){
+    return(
+      <div className='App'>
+        <h1>crystalline</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
